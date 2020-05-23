f3e781a7b9910337b8c0aa5cced087f73ae97683
georginklv <georginklv@abv.bg>
2020-05-23 11:36:47 +0300

    implement Experience page

---
+++ src/components/Experience.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    <div class="experience">
+      <div class="left-colum-experience">
+        <p class="small-title">Qualification</p>
+        <h2 class="heading heading-education">My Education</h2>
+        <div class="positions">
+          <p class="position-small-title">Masters</p>
+          <h2 class="heading-position">Economical University</h2>
+          <p class="date">09.2017 - Present</p>
+          <p class="paragraph">
+            Master in "Business information systems" - Creating Web Apps with
+            HTML5, CSS3, JavaScript, jQuery - Graphic Design with Adobe product
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="right-colum-experience"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Experience",
+  props: {}
+};
+</script>
+
+<style scoped lang="stylus"></style>
